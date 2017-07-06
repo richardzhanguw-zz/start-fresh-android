420100d5f48e0ea6bb54408657ba4471238cd849
Richard Zhang <zhang.richard2016@gmail.com>
2017-07-05 23:30:39 -0400

    initial firebase setup

--- app/src/main/java/com/androidapp/richard/startfresh/Fragments/HealthyCounterFragment.java
+++ app/src/main/java/com/androidapp/richard/startfresh/Fragments/HealthyCounterFragment.java
@@ -16,8 +16,17 @@ import android.widget.TextView;
 
 import com.androidapp.richard.startfresh.Activities.FoodTypeSelectionActivity;
 import com.androidapp.richard.startfresh.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 public class HealthyCounterFragment extends Fragment implements  View.OnClickListener {
 
@@ -45,6 +54,7 @@ public class HealthyCounterFragment extends Fragment implements  View.OnClickLis
         grains.setOnClickListener(this);
         meat.setOnClickListener(this);
         other.setOnClickListener(this);
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
         return view;
     }
 
