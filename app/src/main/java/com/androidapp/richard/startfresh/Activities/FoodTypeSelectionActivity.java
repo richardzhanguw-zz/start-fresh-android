570c97e64187a158d417293525bb08327cf41ae4
Richard Zhang <zhang.richard2016@gmail.com>
2017-07-09 16:15:01 -0400

    draft implementation of food search bar filter

--- app/src/main/java/com/androidapp/richard/startfresh/Activities/FoodTypeSelectionActivity.java
+++ app/src/main/java/com/androidapp/richard/startfresh/Activities/FoodTypeSelectionActivity.java
@@ -2,10 +2,13 @@ package com.androidapp.richard.startfresh.Activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.androidapp.richard.startfresh.R;
@@ -19,12 +22,12 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class FoodTypeSelectionActivity extends Activity {
-    ListView foodSelectionList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_food_type_selection);
-        foodSelectionList = (ListView) findViewById(R.id.food_selection_list_view);
+        final ListView foodSelectionList = (ListView) findViewById(R.id.food_selection_list_view);
+        EditText searchTextField = (EditText) findViewById(R.id.search_foods_edit_text);
         final ArrayList<String> listOfFoods = new ArrayList<String>();
         FirebaseApp.initializeApp(this);
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
@@ -38,7 +41,6 @@ public class FoodTypeSelectionActivity extends Activity {
                     } else {
                         for (DataSnapshot child : dataSnapshot.getChildren()) {
                             listOfFoods.add(child.getValue().toString());
-//                            Log.d("ondatachange", child.getValue().toString());
                         }
                     }
                 }
@@ -48,7 +50,7 @@ public class FoodTypeSelectionActivity extends Activity {
 
                 }
             });
-            ArrayAdapter adapter = new ArrayAdapter(this,
+            final ArrayAdapter adapter = new ArrayAdapter(this,
                     android.R.layout.simple_list_item_1, listOfFoods);
             foodSelectionList.setAdapter(adapter);
             foodSelectionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -57,7 +59,36 @@ public class FoodTypeSelectionActivity extends Activity {
 
                 }
             });
+            searchTextField.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                    String currentText = charSequence.toString();
+//                    ArrayList<String> originalList = listOfFoods;
+//                    listOfFoods.clear();
+//                    Log.d("liststuff", "ontextchanged with size" + String.valueOf(listOfFoods.size()));
+//                    for (int j = 0; i< originalList.size(); i ++){
+//                        Log.d("liststuff", "for loop");
+//                        if(currentText.equals(originalList.get(j).substring(0, i2))){
+//                            listOfFoods.add(originalList.get(j));
+//                            Log.d("liststuff", "for loop if");
+//                        }
+//                    }
+//                    adapter.notifyDataSetChanged();
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
         }
+
     }
 
 
