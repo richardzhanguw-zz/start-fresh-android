33b85bdf1eaa5fc11227476fca2b93a44b416569
Richard Zhang <zhang.richard2016@gmail.com>
2017-08-20 13:36:39 -0400

    UI fixes for add new spending item dialog box and positioning

--- app/src/main/java/com/androidapp/richard/startfresh/Fragments/SpendingTracker.java
+++ app/src/main/java/com/androidapp/richard/startfresh/Fragments/SpendingTracker.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.androidapp.richard.startfresh.AdaptersAndOtherClasses.SpendingTrackerItem;
